5f0759e402d75daf502d38ce5927b1ac0e378580
Dani Jin <danfeng233@gmail.com>
2020-12-04 09:18:41 -0800

    set up functions

---
+++ yahtzee.py
@@ -0,0 +1,27 @@
+
+def SCORE_CARD() -> dict:
+    """
+    Return the Yahtzee score card.
+    """
+    pass
+
+
+def start_game() -> None:  # do we need to write None?
+    """Start the game"""
+    pass
+
+
+def set_players(score_card: dict) -> dict:
+    """
+    Ask the players name and return a dictionary containing the information of the two players.
+
+    :param score_card:
+    :precondition:
+    :postcondition:
+    :return:
+    """
+    pass
+
+
+def start_game():
+    """start the game"""
