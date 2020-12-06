178d182c7036a335a3b8f345f945b8f4325f9e0b
Dani Jin <danfeng233@gmail.com>
2020-12-05 19:54:58 -0800

    refactor the parameter

--- test_main_menu.py
+++ test_main_menu.py
@@ -7,9 +7,9 @@ import io
 @patch('sys.stdout', new_callable=io.StringIO)
 class Test(TestCase):
 
-    def test_main_menu_unavailable_choice_empty_dice_time_1(self, mock_stdout):
-        unavailable_choice = ([], 1)
-        main_menu(unavailable_choice)
+    def test_main_menu_unavailable_options_empty_dice_time_1(self, mock_stdout):
+        unavailable_options = ([], 1)
+        main_menu(unavailable_options)
         expected = "What do you want to do?\n" \
                    "\033[1;32m(1) - Keep the dice\033[0m\n" \
                    "\033[1;32m(2) - Move the dice\033[0m\n" \
@@ -18,9 +18,9 @@ class Test(TestCase):
                    "\033[1;32m(5) - write score\033[0m\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
-    def test_main_menu_unavailable_choice_empty_dice_time_2(self, mock_stdout):
-        unavailable_choice = ([], 2)
-        main_menu(unavailable_choice)
+    def test_main_menu_unavailable_options_empty_dice_time_2(self, mock_stdout):
+        unavailable_options = ([], 2)
+        main_menu(unavailable_options)
         expected = "What do you want to do?\n" \
                    "\033[1;32m(1) - Keep the dice\033[0m\n" \
                    "\033[1;32m(2) - Move the dice\033[0m\n" \
@@ -29,9 +29,9 @@ class Test(TestCase):
                    "\033[1;32m(5) - write score\033[0m\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
-    def test_main_menu_unavailable_choice_3_dice_time_0(self, mock_stdout):
-        unavailable_choice = ([3], 0)
-        main_menu(unavailable_choice)
+    def test_main_menu_unavailable_options_3_dice_time_0(self, mock_stdout):
+        unavailable_options = ([3], 0)
+        main_menu(unavailable_options)
         expected = "What do you want to do?\n" \
                    "\033[1;32m(1) - Keep the dice\033[0m\n" \
                    "\033[1;32m(2) - Move the dice\033[0m\n" \
@@ -40,9 +40,9 @@ class Test(TestCase):
                    "\033[1;32m(5) - write score\033[0m\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
-    def test_main_unavailable_choice_1_3_dice_time_0(self, mock_stdout):
-        unavailable_choice = ([1, 3], 0)
-        main_menu(unavailable_choice)
+    def test_main_unavailable_options_1_3_dice_time_0(self, mock_stdout):
+        unavailable_options = ([1, 3], 0)
+        main_menu(unavailable_options)
         expected = "What do you want to do?\n" \
                    "\033[1;37m(1) - Keep the dice\033[0m\n" \
                    "\033[1;32m(2) - Move the dice\033[0m\n" \
@@ -51,9 +51,9 @@ class Test(TestCase):
                    "\033[1;32m(5) - write score\033[0m\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
-    def test_main_unavailable_choice_1_3_dice_time_2(self, mock_stdout):
-        unavailable_choice = ([1, 3], 2)
-        main_menu(unavailable_choice)
+    def test_main_unavailable_options_1_3_dice_time_2(self, mock_stdout):
+        unavailable_options = ([1, 3], 2)
+        main_menu(unavailable_options)
         expected = "What do you want to do?\n" \
                    "\033[1;37m(1) - Keep the dice\033[0m\n" \
                    "\033[1;32m(2) - Move the dice\033[0m\n" \
