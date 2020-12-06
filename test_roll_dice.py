c141dbdf55d53d607422a7307313acedca89ed8f
Dani Jin <danfeng233@gmail.com>
2020-12-05 20:29:15 -0800

    Finished

--- test_roll_dice.py
+++ test_roll_dice.py
@@ -1,6 +1,41 @@
 from unittest import TestCase
+from yahtzee import roll_dice
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_roll_dice(self):
-        self.fail()
+
+    @patch('random.sample', return_value=["1", "2", "4", "5", "6"])
+    def test_roll_dice_kept_dice_length_0(self, mock_output):
+        kept_dice = []
+        result = roll_dice(kept_dice)
+        expected = ["1", "2", "4", "5", "6"]
+        self.assertEqual(result, expected)
+
+    @patch('random.sample', return_value=["2", "4", "5", "6"])
+    def test_roll_dice_kept_dice_length_1(self, mock_output):
+        kept_dice = ["1"]
+        result = roll_dice(kept_dice)
+        expected = ["2", "4", "5", "6"]
+        self.assertEqual(result, expected)
+
+    @patch('random.sample', return_value=["1", "2", "4"])
+    def test_roll_dice_kept_dice_length_2(self, mock_output):
+        kept_dice = ["1", "3"]
+        result = roll_dice(kept_dice)
+        expected = ["1", "2", "4"]
+        self.assertEqual(result, expected)
+
+    @patch('random.sample', return_value=["1", "3"])
+    def test_roll_dice_kept_dice_length_3(self, mock_output):
+        kept_dice = ["1", "3", "5"]
+        result = roll_dice(kept_dice)
+        expected = ["1", "3"]
+        self.assertEqual(result, expected)
+
+    @patch('random.sample', return_value=["6"])
+    def test_roll_dice_kept_dice_length_4(self, mock_output):
+        kept_dice = ["1", "3", "5", "1"]
+        result = roll_dice(kept_dice)
+        expected = ["6"]
+        self.assertEqual(result, expected)
