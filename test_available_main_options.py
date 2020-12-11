083a9ea70411b2430f4c558db859b6876101240a
Dani Jin <danfeng233@gmail.com>
2020-12-11 13:58:31 -0800

    switch actual and expected

--- test_available_main_options.py
+++ test_available_main_options.py
@@ -6,48 +6,48 @@ class TestAvailableMainOptions(TestCase):
     def test_available_main_options_dice_time_2_kept_dice_length_0(self):
         dice_time = 2
         kept_dice = []
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"1", "3", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"1", "3", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_2_kept_dice_length_3(self):
         dice_time = 2
         kept_dice = [2, 3, 4]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"1", "2", "3", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"1", "2", "3", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_2_kept_dice_length_5(self):
         dice_time = 2
         kept_dice = [1, 2, 3, 4, 5]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"2", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"2", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_1_kept_dice_length_2(self):
         dice_time = 1
         kept_dice = [3, 1, 1]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"1", "2", "3", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"1", "2", "3", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_1_kept_dice_length_5(self):
         dice_time = 1
         kept_dice = [3, 1, 1, 3, 4]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"2", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"2", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_0_kept_dice_length_2(self):
         dice_time = 0
         kept_dice = [3, 1, 1]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"1", "2", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"1", "2", "4", "5"}
+        self.assertEqual(actual, expected)
 
     def test_available_main_options_dice_time_0_kept_dice_length_5(self):
         dice_time = 0
         kept_dice = [3, 1, 1, 1, 1]
-        expected = available_main_options(dice_time, kept_dice)
-        actual = {"2", "4", "5"}
-        self.assertEqual(expected, actual)
+        actual = available_main_options(dice_time, kept_dice)
+        expected = {"2", "4", "5"}
+        self.assertEqual(actual, expected)
